69578f7335cb94158a12dcbfd4e748665925d575
Md Jahirul Islam <mdjoherul50@gmail.com>
2024-02-04 23:55:13 +0600

    chnages

--- best-wp-testimonials/best-wp-testimonials.php
+++ best-wp-testimonials/best-wp-testimonials.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
  * Plugin Name:       Best WP Testimonials
@@ -18,82 +18,143 @@
 /**
  * bwpt styles css
  */
-function bwpt_enqueue_style() {
-   //wp_enqueue_style('string $handle', mixed $src, array $deps, mixed $ver, string $meida );
-	wp_enqueue_style( 'owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css' );
-    wp_enqueue_style( 'owl.theme', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css' );
-    wp_enqueue_style( 'bwpt-style', plugins_url( 'css/bwpt-style.css', __FILE__ ) );
-	
+function bwpt_enqueue_style()
+{
+    //wp_enqueue_style('string $handle', mixed $src, array $deps, mixed $ver, string $meida );
+    wp_enqueue_style('owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css');
+    wp_enqueue_style('owl.theme', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css');
+    wp_enqueue_style('bwpt-style', plugins_url('css/bwpt-style.css', __FILE__));
+
 }
-add_action( 'wp_enqueue_scripts', 'bwpt_enqueue_style');
+add_action('wp_enqueue_scripts', 'bwpt_enqueue_style');
 
 /* bwpt js style add*/
-function bwpt_enqueue_script(){
+function bwpt_enqueue_script()
+{
     //wp_enqueue_style('string $handle', mixed $src, array $deps, mixed $ver, bol $in_footer );
-    wp_enqueue_script('adsensejs', 'https://pagead2.googlesyndication.com/pagead/managed/js/adsense/m202401110101/show_ads_impl_fy2021.js?bust=31080431', array(), '1.0.0', 'true' );
-    wp_enqueue_script('owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array(), '1.0.0', 'true' );
-    wp_enqueue_script('bwpt_script', plugins_url( 'js/bwpt_script.js', __FILE__ ), array(), '1.0.0', 'true' );
+    wp_enqueue_script('adsensejs', 'https://pagead2.googlesyndication.com/pagead/managed/js/adsense/m202401110101/show_ads_impl_fy2021.js?bust=31080431', array(), '1.0.0', 'true');
+    wp_enqueue_script('owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array(), '1.0.0', 'true');
+    wp_enqueue_script('bwpt_script', plugins_url('js/bwpt_script.js', __FILE__), array(), '1.0.0', 'true');
 }
 add_action('wp_enqueue_scripts', 'bwpt_enqueue_script');
 
 // Register Custom Post Type
-if ( ! function_exists('bwpt_custom_post_type') ) {
+if (!function_exists('bwpt_custom_post_type')) {
 
     // Register Custom Post Type
-    function bwpt_custom_post_type() {
-    
+    function bwpt_custom_post_type()
+    {
+
         $labels = array(
-            'name'                  => _x( 'Testimonials', 'Post Type General Name', 'bwpt' ),
-            'singular_name'         => _x( 'Testimonial Type', 'Post Type Singular Name', 'bwpt' ),
-            'menu_name'             => __( 'Testimonials', 'bwpt' ),
-            'name_admin_bar'        => __( 'Post Type', 'bwpt' ),
-            'archives'              => __( 'Item Archives', 'bwpt' ),
-            'attributes'            => __( 'Item Attributes', 'bwpt' ),
-            'parent_item_colon'     => __( 'Parent Item:', 'bwpt' ),
-            'all_items'             => __( 'All Items', 'bwpt' ),
-            'add_new_item'          => __( 'Add New Item', 'bwpt' ),
-            'add_new'               => __( 'Add New', 'bwpt' ),
-            'new_item'              => __( 'New Item', 'bwpt' ),
-            'edit_item'             => __( 'Edit Item', 'bwpt' ),
-            'update_item'           => __( 'Update Item', 'bwpt' ),
-            'view_item'             => __( 'View Item', 'bwpt' ),
-            'view_items'            => __( 'View Items', 'bwpt' ),
-            'search_items'          => __( 'Search Item', 'bwpt' ),
-            'not_found'             => __( 'Not found', 'bwpt' ),
-            'not_found_in_trash'    => __( 'Not found in Trash', 'bwpt' ),
-            'featured_image'        => __( 'Featured Image', 'bwpt' ),
-            'set_featured_image'    => __( 'Set featured image', 'bwpt' ),
-            'remove_featured_image' => __( 'Remove featured image', 'bwpt' ),
-            'use_featured_image'    => __( 'Use as featured image', 'bwpt' ),
-            'insert_into_item'      => __( 'Insert into item', 'bwpt' ),
-            'uploaded_to_this_item' => __( 'Uploaded to this item', 'bwpt' ),
-            'items_list'            => __( 'Items list', 'bwpt' ),
-            'items_list_navigation' => __( 'Items list navigation', 'bwpt' ),
-            'filter_items_list'     => __( 'Filter items list', 'bwpt' ),
+            'name' => _x('Testimonials', 'Post Type General Name', 'bwpt'),
+            'singular_name' => _x('Testimonial Type', 'Post Type Singular Name', 'bwpt'),
+            'menu_name' => __('Testimonials', 'bwpt'),
+            'name_admin_bar' => __('Post Type', 'bwpt'),
+            'archives' => __('Item Archives', 'bwpt'),
+            'attributes' => __('Item Attributes', 'bwpt'),
+            'parent_item_colon' => __('Parent Item:', 'bwpt'),
+            'all_items' => __('All Items', 'bwpt'),
+            'add_new_item' => __('Add New Item', 'bwpt'),
+            'add_new' => __('Add New', 'bwpt'),
+            'new_item' => __('New Item', 'bwpt'),
+            'edit_item' => __('Edit Item', 'bwpt'),
+            'update_item' => __('Update Item', 'bwpt'),
+            'view_item' => __('View Item', 'bwpt'),
+            'view_items' => __('View Items', 'bwpt'),
+            'search_items' => __('Search Item', 'bwpt'),
+            'not_found' => __('Not found', 'bwpt'),
+            'not_found_in_trash' => __('Not found in Trash', 'bwpt'),
+            'featured_image' => __('Featured Image', 'bwpt'),
+            'set_featured_image' => __('Set featured image', 'bwpt'),
+            'remove_featured_image' => __('Remove featured image', 'bwpt'),
+            'use_featured_image' => __('Use as featured image', 'bwpt'),
+            'insert_into_item' => __('Insert into item', 'bwpt'),
+            'uploaded_to_this_item' => __('Uploaded to this item', 'bwpt'),
+            'items_list' => __('Items list', 'bwpt'),
+            'items_list_navigation' => __('Items list navigation', 'bwpt'),
+            'filter_items_list' => __('Filter items list', 'bwpt'),
         );
         $args = array(
-            'label'                 => __( 'Testimonial Type', 'bwpt' ),
-            'description'           => __( 'Testimonial Description', 'bwpt' ),
-            'labels'                => $labels,
-            'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
-            'hierarchical'          => false,
-            'public'                => true,
-            'show_ui'               => true,
-            'show_in_menu'          => true,
-            'menu_position'         => 999,
-            'show_in_admin_bar'     => true,
-            'show_in_nav_menus'     => true,
-            'can_export'            => true,
-            'has_archive'           => true,
-            'exclude_from_search'   => false,
-            'publicly_queryable'    => true,
-            'capability_type'       => 'page',
+            'label' => __('Testimonial Type', 'bwpt'),
+            'description' => __('Testimonial Description', 'bwpt'),
+            'labels' => $labels,
+            'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
+            'hierarchical' => false,
+            'public' => true,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'menu_position' => 999,
+            'show_in_admin_bar' => true,
+            'show_in_nav_menus' => true,
+            'can_export' => true,
+            'has_archive' => true,
+            'exclude_from_search' => false,
+            'publicly_queryable' => true,
+            'capability_type' => 'page',
         );
-        register_post_type( 'testimonial', $args );
-    
+        register_post_type('testimonial', $args);
+
     }
-    add_action( 'init', 'bwpt_custom_post_type', 0 );
-    
+    add_action('init', 'bwpt_custom_post_type', 0);
+
+    function bwpt_testimonial_loop(){
+     ?> 
+     <div id="testimonial-slider" class="owl-carousel">
+     <?php
+        $args = array(
+            'post_type' => array('testimonial'),
+            'post_status' => array('publish'),
+            // 'post_per_page' => 5,
+        );
+
+        // The Query
+        $bwpt_query = new WP_Query($args);
+
+        // The Loop
+        if ($bwpt_query->have_posts()) {
+            while ($bwpt_query->have_posts()) {
+                $bwpt_query->the_post();
+                ?>
+                <div class="testimonial">
+                    <div class="pic">
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" alt="<?php the_title()?>">
+                    </div>
+                    <h3 class="title"><?php the_title()?></h3>
+                    <p class="description"><?php the_excerpt() ?></p>
+                    <div class="testimonial-content">
+                        <div class="testimonial-profile">
+                            <h3 class="name"><?php echo get_post_meta( get_the_ID(), 'testi_name',true)?></h3>
+                            <span class="post"><?php echo get_post_meta( get_the_ID(), 'testi_desi',true )?></span>
+                        </div>
+                        <ul class="rating">
+                            <li class="fa fa-star"></li>
+                            <li class="fa fa-star"></li>
+                            <li class="fa fa-star"></li>
+                            <li class="fa fa-star"></li>
+                            <li class="fa fa-star-half-empty"></li>
+                        </ul>
+                    </div>
+                </div>
+            <?php }
+        } else {
+            // no posts found
+        }
+
+        // Restore original Post Data
+        wp_reset_postdata();
+        ?>
+      </div>
+      <?php
+      
     }
+    function bwpt_add_custom_shortcode() {
+        add_shortcode( 'TESTIMONIALS', 'bwpt_testimonial_loop' );
+    }
+    add_action( 'init', 'bwpt_add_custom_shortcode' );
+}
+//bwpt shortcode
+
+
+
 
 ?>
