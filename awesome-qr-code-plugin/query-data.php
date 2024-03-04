24572c358b2aadfce007ac21ec2db2ae4008fde9
Md Jahirul Islam <mdjoherul50@gmail.com>
2024-03-04 23:52:45 +0600

    three types of query data wp_qyery is best

--- awesome-qr-code-plugin/query-data.php
+++ awesome-qr-code-plugin/query-data.php
@@ -5,8 +5,10 @@
  */
 class Query_Data{
   public function __construct(){
-    //   add_shortcode('query_data', array($this,'query_data'));
-         add_shortcode('query_data', array($this,'wp_query'));
+    //  add_shortcode('query_data', array($this,'query_data'));
+    //  add_shortcode('query_data', array($this,'wp_query'));
+        add_shortcode('query_data', array($this,'wp_db'));
+    //  add_shortcode('query_data', array($this,'get_post'));
   }
    
   /*
@@ -37,6 +39,7 @@ class Query_Data{
   }
    */
 
+  /*
    public function wp_query(){
      ob_start();
      $args = array(
@@ -62,5 +65,35 @@ class Query_Data{
   wp_reset_postdata();
   return ob_get_clean();
 
+
+}
+*/
+public function wp_db(){
+  ob_start();
+  global $wpdb;
+  $results = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_type='post' AND post_status='publish' LIMIT 5 ");
+  ?>
+ <ul>
+    <?php
+    foreach ($results as $post){
+        ?>
+    <li><a href="<?php echo get_permalink($post->ID);?>"><?php echo $post->post_title; ?></a></li>
+    <p>  <?php echo $post->post_content  ?></p>
+
+    <?php 
+
+    }
+    ?>
+    </ul>
+    <?php
+    return ob_get_clean();
+
+}
+//get post er madommde specific post er data queru kora jay
+public function get_post(){
+  $post = get_post(1);
+
+  return "<h1>$post->post_title</h1> <p>$post->post_content</p>";
+}
 
 }
